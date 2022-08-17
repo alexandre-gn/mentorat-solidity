857788866ececda6dba4e06aa6a1c43055455034
Alexandre GN <alexandre-gn@outlook.com>
2022-08-17 06:38:28 +0200

    Keep hardhat as default network in config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ const { API_URL, PRIVATE_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
-  defaultNetwork: "rinkeby",
+  defaultNetwork: "hardhat",
   networks: {
    hardhat: {},
     rinkeby: {
